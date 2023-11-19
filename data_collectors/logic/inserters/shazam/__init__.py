fc9a427adf6fd047e6ff6ceef30b23cccf32f06f
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-11-19 21:10:20 +0200

    Rename shazam tracks inserter

--- data_collectors/logic/inserters/shazam/__init__.py
+++ data_collectors/logic/inserters/shazam/__init__.py
@@ -1,5 +1,5 @@
-from data_collectors.logic.inserters.shazam.shazam_top_tracks_database_inserter import ShazamTopTracksDatabaseInserter
+from data_collectors.logic.inserters.shazam.shazam_tracks_database_inserter import ShazamTracksDatabaseInserter
 
 __all__ = [
-    "ShazamTopTracksDatabaseInserter"
+    "ShazamTracksDatabaseInserter"
 ]
