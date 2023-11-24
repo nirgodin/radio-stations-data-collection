0b43a07b59c9471015c46e4394c3ca5c8827dd3d
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-11-24 19:28:56 +0200

    Moved artists ids extraction out of artists collector

--- data_collectors/logic/collectors/shazam/shazam_artists_collector.py
+++ data_collectors/logic/collectors/shazam/shazam_artists_collector.py
@@ -1,22 +1,19 @@
-from typing import Any, List, Dict, Optional, Tuple
+from typing import Any, List, Optional, Tuple
 
 from shazamio.schemas.artists import ArtistQuery
 from shazamio.schemas.enums import ArtistView, ArtistExtend
 
-from data_collectors.consts.shazam_consts import ADAM_ID, KEY
-from data_collectors.consts.spotify_consts import ARTISTS
 from data_collectors.logic.collectors.shazam.base_shazam_collector import BaseShazamCollector
 from data_collectors.logic.utils import get_all_enum_values
 from data_collectors.logs import logger
 
 
 class ShazamArtistsCollector(BaseShazamCollector):
-    async def collect(self, tracks: List[dict]) -> Any:
-        logger.info(f"Starting to collect Shazam artists for {len(tracks)} tracks")
-        artists_ids = {self._extract_artist_id(track) for track in tracks}
-        results = await self._pool_executor.run(iterable=artists_ids, func=self._extract_single_track_artist)
+    async def collect(self, ids: List[str]) -> Any:
+        logger.info(f"Starting to collect {len(ids)} Shazam artists")
+        results = await self._pool_executor.run(iterable=ids, func=self._extract_single_track_artist)
         valid_results = [result for result in results if isinstance(result, dict)]
-        logger.info(f"Successfully retrieved {len(valid_results)} Shazam artists from {len(tracks)} tracks")
+        logger.info(f"Successfully retrieved {len(valid_results)} Shazam artists from {len(ids)} tracks")
 
         return valid_results
 
@@ -32,14 +29,3 @@ class ShazamArtistsCollector(BaseShazamCollector):
                 extend=get_all_enum_values(ArtistExtend)
             )
         )
-        # track_id = track[KEY]
-        #
-        # return {track_id: response}
-
-    @staticmethod
-    def _extract_artist_id(track: dict) -> Optional[str]:
-        artists = track.get(ARTISTS, [])
-
-        if artists:
-            first_artist = artists[0]
-            return first_artist.get(ADAM_ID)
--- data_collectors/logic/inserters/shazam_insertions_manager.py
+++ data_collectors/logic/inserters/shazam_insertions_manager.py
@@ -1,7 +1,9 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from postgres_client import BaseORMModel
 
+from data_collectors.consts.shazam_consts import ADAM_ID
+from data_collectors.consts.spotify_consts import ARTISTS
 from data_collectors.logic.collectors import ShazamArtistsCollector, ShazamTracksCollector
 from data_collectors.logic.inserters.shazam.shazam_artists_database_inserter import ShazamArtistsDatabaseInserter
 from data_collectors.logic.inserters.shazam.shazam_tracks_database_inserter import ShazamTracksDatabaseInserter
@@ -34,7 +36,16 @@ class ShazamInsertionsManager:
     async def _fetch_raw_records(self, ids: List[str]) -> List[List[dict]]:
         logger.info(f"Starting to collect tracks and artists from shazam for {len(ids)} tracks ids")
         tracks = await self._tracks_collector.collect(ids)
-        artists = await self._artists_collector.collect(tracks)
+        artists_ids = {self._extract_artist_id(track) for track in tracks}
+        artists = await self._artists_collector.collect(list(artists_ids))
         logger.info("Successfully retrieved tracks and artists from shazam")
 
         return [artists, tracks]
+
+    @staticmethod
+    def _extract_artist_id(track: dict) -> Optional[str]:
+        artists = track.get(ARTISTS, [])
+
+        if artists:
+            first_artist = artists[0]
+            return first_artist.get(ADAM_ID)
