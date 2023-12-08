28fdfe9cd48efda0360f2e54bb2904f887bafd21
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-12-08 19:44:48 +0200

    Removed bad imports of collector from consts init

--- data_collectors/consts/__init__.py
+++
@@ -1,8 +0,0 @@
-from data_collectors.logic.collectors.billboard.billboard_charts_collector import BillboardChartsCollector
-from data_collectors.logic.collectors.billboard.billboard_tracks_collector import BillboardTracksCollector
-
-__all__ = [
-    "BillboardChartsCollector",
-    "BillboardTracksCollector"
-]
-
