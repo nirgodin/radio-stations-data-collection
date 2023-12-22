94eb7a3d5161d95d32e1cf7891c1f71ff8c0c594
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-12-22 17:10:13 +0200

    Upgraded genie-datastores and added birth date source to wikipedia mangers

--- data_collectors/logic/models/artist_wikipedia_details.py
+++ data_collectors/logic/models/artist_wikipedia_details.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
 
-from genie_datastores.postgres.models import Artist
+from genie_datastores.postgres.models import Artist, DataSource
 
 from data_collectors.logic.models.database_update_request import DBUpdateRequest
 
@@ -14,10 +14,15 @@ class ArtistWikipediaDetails:
     death_date: Optional[datetime]
 
     def to_update_request(self) -> DBUpdateRequest:
+        values = {
+            Artist.birth_date: self.birth_date,
+            Artist.death_date: self.death_date
+        }
+
+        if self.birth_date is not None or self.death_date is not None:
+            values[Artist.birth_date_source] = DataSource.GENERAL_WIKIPEDIA
+
         return DBUpdateRequest(
             id=self.id,
-            values={
-                Artist.birth_date: self.birth_date,
-                Artist.death_date: self.death_date
-            }
+            values=values
         )
