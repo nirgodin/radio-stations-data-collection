bc267d616a0448d7becd3b78cbb01804dc21dd20
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-02-23 16:54:30 +0200

    Added spacy and spacy-langdetect to create language_detector.py

--- data_collectors/tools/__init__.py
+++ data_collectors/tools/__init__.py
@@ -1,7 +1,9 @@
 from data_collectors.tools.image_detection.image_gender_detector import ImageGenderDetector
+from data_collectors.tools.language_detector import LanguageDetector
 from data_collectors.tools.web_elements_extractor import WebElementsExtractor
 
 __all__ = [
     "ImageGenderDetector",
+    "LanguageDetector",
     "WebElementsExtractor"
 ]
