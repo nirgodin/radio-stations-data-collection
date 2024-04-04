df9fe11ed04bf430837682516207cbe48ef14785
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-04-04 10:06:25 +0300

    Integrated chunks inserter in billboard charts inserter

--- data_collectors/components/inserters/billboard_inserters_component_factory.py
+++ data_collectors/components/inserters/billboard_inserters_component_factory.py
@@ -1,12 +1,13 @@
 from genie_datastores.postgres.operations import get_database_engine
 
-from data_collectors.logic.inserters.postgres import BillboardChartsDatabaseInserter, BillboardTracksDatabaseInserter
+from data_collectors.logic.inserters.postgres import BillboardChartsDatabaseInserter, BillboardTracksDatabaseInserter, \
+    ChunksDatabaseInserter
 
 
 class BillboardInsertersComponentFactory:
     @staticmethod
-    def get_charts_inserter() -> BillboardChartsDatabaseInserter:
-        return BillboardChartsDatabaseInserter(get_database_engine())
+    def get_charts_inserter(chunks_inserter: ChunksDatabaseInserter) -> BillboardChartsDatabaseInserter:
+        return BillboardChartsDatabaseInserter(chunks_inserter)
 
     @staticmethod
     def get_tracks_inserter() -> BillboardTracksDatabaseInserter:
--- data_collectors/logic/inserters/postgres/billboard/billboard_charts_database_inserter.py
+++ data_collectors/logic/inserters/postgres/billboard/billboard_charts_database_inserter.py
@@ -6,12 +6,17 @@ from genie_datastores.postgres.operations import insert_records
 from data_collectors.contract.inserters.postgres_database_inserter_interface import IPostgresDatabaseInserter
 from genie_common.tools import logger
 
+from data_collectors.logic.inserters.postgres import ChunksDatabaseInserter
 
-class BillboardChartsDatabaseInserter(IPostgresDatabaseInserter):
+
+class BillboardChartsDatabaseInserter:
+    def __init__(self, chunks_inserter: ChunksDatabaseInserter):
+        self._chunks_inserter = chunks_inserter
+
     async def insert(self, charts_entries: List[ChartEntryData]) -> List[BillboardChartEntry]:
         logger.info(f"Starting to insert {len(charts_entries)} Billboard chart entries")
         records = [BillboardChartEntry.from_chart_entry(entry) for entry in charts_entries]
-        await insert_records(engine=self._db_engine, records=records)
+        await self._chunks_inserter.insert(records)
         logger.info(f"Successfully inserted Billboard chart entries")
 
         return records
