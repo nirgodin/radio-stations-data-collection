8db9bde2598b406dd8838a54fe26c8cd8c3e9bd3
nir.godin <nir.godin@planckresolution.com>
2024-04-29 17:17:14 +0300

    Created tracks vectorizer data collector

--- data_collectors/components/collectors/collectors_component_factory.py
+++ data_collectors/components/collectors/collectors_component_factory.py
@@ -2,6 +2,7 @@ from data_collectors.components.collectors.billboard_collectors_component_factor
     BillboardCollectorsComponentFactory
 from data_collectors.components.collectors.genius_collectors_component_factory import GeniusCollectorsComponentFactory
 from data_collectors.components.collectors.google_collectors_component_factory import GoogleCollectorsComponentFactory
+from data_collectors.components.collectors.miscellaneous_collectors_factory import MiscellaneousCollectorsFactory
 from data_collectors.components.collectors.musixmatch_collectors_component_factory import \
     MusixmatchCollectorsComponentFactory
 from data_collectors.components.collectors.openai_collectors_component_factory import OpenAICollectorsComponentFactory
@@ -19,6 +20,7 @@ class CollectorsComponentFactory:
                  charts: ChartsCollectorsComponentFactory = ChartsCollectorsComponentFactory(),
                  genius: GeniusCollectorsComponentFactory = GeniusCollectorsComponentFactory(),
                  google: GoogleCollectorsComponentFactory = GoogleCollectorsComponentFactory(),
+                 misc: MiscellaneousCollectorsFactory = MiscellaneousCollectorsFactory(),
                  musixmatch: MusixmatchCollectorsComponentFactory = MusixmatchCollectorsComponentFactory(),
                  openai: OpenAICollectorsComponentFactory = OpenAICollectorsComponentFactory(),
                  shazam: ShazamCollectorsComponentFactory = ShazamCollectorsComponentFactory(),
@@ -28,6 +30,7 @@ class CollectorsComponentFactory:
         self.charts = charts
         self.genius = genius
         self.google = google
+        self.misc = misc
         self.musixmatch = musixmatch
         self.openai = openai
         self.shazam = shazam
