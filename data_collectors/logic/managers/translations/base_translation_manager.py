30c77cd93f4024159f18fa80e39a11c5bd5399ee
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-05-25 18:54:02 +0300

    Added cache to translations

--- data_collectors/logic/managers/translations/base_translation_manager.py
+++ data_collectors/logic/managers/translations/base_translation_manager.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from typing import Optional, Dict, Tuple, Type
+from typing import Optional, Dict, Tuple, Type, List, Awaitable, Callable
 
 from database.orm_models.base_orm_model import BaseORMModel
 from genie_common.clients.google import GoogleTranslateClient
@@ -60,23 +60,48 @@ class BaseTranslationManager(IManager):
 
     async def _translate_single_record(self, entity_id_and_name: Tuple[str, str]) -> Translation:
         entity_id, entity_name = entity_id_and_name
+
+        for translation_method in self._prioritized_translation_methods:
+            translation = await translation_method(entity_name)
+
+            if translation is not None:
+                return Translation(
+                    id=entity_id,
+                    entity_source=self._entity_source,
+                    entity_type=self._entity_type,
+                    text=entity_name,
+                    translation=translation,
+                    source_language="en",
+                    target_language="he"
+                )
+
+    async def _retrieve_translation_from_cache(self, text: str) -> Optional[str]:
+        query = (
+            select(Translation.translation)
+            .where(Translation.text.ilike(text))
+            .limit(1)
+        )
+        query_result = await execute_query(engine=self._db_engine, query=query)
+
+        return query_result.scalars().first()
+
+    async def _fetch_translation(self, text: str) -> Optional[str]:
         translation_response = await self._translation_client.translate(
-            texts=[entity_name],
+            texts=[text],
             target_language="he",
             source_language="en"
         )
 
         if translation_response:
             first_translation = translation_response[0]
-            return Translation(
-                id=entity_id,
-                entity_source=self._entity_source,
-                entity_type=self._entity_type,
-                text=entity_name,
-                translation=first_translation.translation,
-                source_language=first_translation.source_language,
-                target_language=first_translation.target_language
-            )
+            return first_translation.translation
+
+    @property
+    def _prioritized_translation_methods(self) -> List[Callable[[str], Awaitable[Optional[str]]]]:
+        return [
+            self._retrieve_translation_from_cache,
+            self._fetch_translation
+        ]
 
     @property
     @abstractmethod
