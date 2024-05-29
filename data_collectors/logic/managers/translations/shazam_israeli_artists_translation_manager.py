c10e76ee9de6c821ea68dbe6a89ade5ee5ee9734
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-05-29 09:06:56 +0300

    Simplified query

--- data_collectors/logic/managers/translations/shazam_israeli_artists_translation_manager.py
+++ data_collectors/logic/managers/translations/shazam_israeli_artists_translation_manager.py
@@ -13,10 +13,7 @@ class ShazamIsraeliArtistsTranslationManager(BaseTranslationManager):
     def _query(self) -> Select:
         return (
             select(ShazamArtist.id, ShazamArtist.name)
-            .where(ShazamArtist.id == ShazamTrack.artist_id)
-            .where(ShazamTrack.id == TrackIDMapping.shazam_id)
-            .where(TrackIDMapping.id == SpotifyTrack.id)
-            .where(SpotifyTrack.artist_id == Artist.id)
+            .where(ShazamArtist.id == Artist.shazam_id)
             .where(Artist.is_israeli.is_(True))
             .where(ShazamArtist.name.regexp_match(r'^[a-zA-Z0-9\s]+$'))
         )
