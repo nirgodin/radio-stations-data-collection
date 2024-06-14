a653f3f63e5717d73886150a5b49789321ae82eb
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-06-14 13:05:43 +0300

    Remove redundant dependencies injection in wikipedia manager factory to

--- data_collectors/components/managers/wikipedia_manager_factory.py
+++ data_collectors/components/managers/wikipedia_manager_factory.py
@@ -4,21 +4,15 @@ from data_collectors.logic.managers import *
 
 class WikipediaManagerFactory(BaseManagerFactory):
     def get_artists_age_name_manager(self) -> WikipediaArtistsAgeManager:
-        pool_executor = self.tools.get_pool_executor()
-        age_collector = self.collectors.wikipedia.get_wikipedia_age_name_collector(pool_executor)
-
         return WikipediaArtistsAgeManager(
-            age_collector=age_collector,
+            age_collector=self.collectors.wikipedia.get_wikipedia_age_name_collector(),
             age_analyzer=self.analyzers.get_wikipedia_age_analyzer(),
             db_updater=self.updaters.get_values_updater()
         )
 
     def get_artists_age_link_manager(self) -> WikipediaArtistsAgeManager:
-        pool_executor = self.tools.get_pool_executor()
-        age_collector = self.collectors.wikipedia.get_wikipedia_age_link_collector(pool_executor)
-
         return WikipediaArtistsAgeManager(
-            age_collector=age_collector,
+            age_collector=self.collectors.wikipedia.get_wikipedia_age_link_collector(),
             age_analyzer=self.analyzers.get_wikipedia_age_analyzer(),
             db_updater=self.updaters.get_values_updater()
         )
