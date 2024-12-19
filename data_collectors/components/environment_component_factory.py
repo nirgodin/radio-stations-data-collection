7151d748b3d7921629c566d7cdff0d3d583d58ab
nir.godin <nir.godin@planckresolution.com>
2024-12-19 19:24:02 +0200

    Support default env vars

--- data_collectors/components/environment_component_factory.py
+++ data_collectors/components/environment_component_factory.py
@@ -1,11 +1,35 @@
 import os
 from functools import lru_cache
-from typing import List
+from typing import List, Dict, Optional
 
 from genie_common.utils import env_var_to_list
+from spotipyio.auth import ClientCredentials
 
 
 class EnvironmentComponentFactory:
+    def __init__(self, default_env: Optional[Dict[str, str]] = None):
+        self._default_env = default_env or {}
+
+    @lru_cache
+    def get_spotify_credentials(self) -> ClientCredentials:
+        return ClientCredentials(
+            client_id=self._lookup_env_var("SPOTIPY_CLIENT_ID"),
+            client_secret=self._lookup_env_var("SPOTIPY_CLIENT_SECRET"),
+            redirect_uri=self._lookup_env_var("SPOTIPY_REDIRECT_URI"),
+        )
+
+    @lru_cache
+    def get_spotify_token_request_url(self) -> str:
+        return self._lookup_env_var("SPOTIPY_TOKEN_REQUEST_URL")
+
+    @lru_cache
+    def get_email_user(self) -> str:
+        return self._lookup_env_var("EMAIL_USER")
+
+    @lru_cache
+    def get_email_password(self) -> str:
+        return self._lookup_env_var("EMAIL_PASSWORD")
+
     @staticmethod
     @lru_cache
     def get_musixmatch_api_key() -> str:
@@ -60,3 +84,9 @@ class EnvironmentComponentFactory:
     @lru_cache
     def get_release_radar_playlist_id() -> str:
         return os.environ["RELEASE_RADAR_PLAYLIST_ID"]
+
+    def _lookup_env_var(self, key: str) -> str:
+        if key in self._default_env.keys():
+            return self._default_env[key]
+
+        return os.environ[key]
