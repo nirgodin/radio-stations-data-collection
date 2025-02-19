4efc29a6aa54a6d7d5e0ed1121d444e25560578b
nir.godin <nir.godin@planckresolution.com>
2025-02-19 09:37:37 +0200

    Add billboard charts job using billboard hot 100 playlist

---
+++ tests/playlists_charts/test_billboard_charts_manager.py
@@ -0,0 +1,27 @@
+from typing import Dict, Type
+
+from _pytest.fixtures import fixture
+from genie_datastores.postgres.models import Chart
+
+from data_collectors.consts.charts_consts import (
+    BILLBOARD_PLAYLIST_CHART_MAP,
+)
+from data_collectors.jobs.job_builders.billboard_chart_job_builder import (
+    BillboardChartJobBuilder,
+)
+from data_collectors.jobs.job_id import JobId
+from tests.playlists_charts.base_playlists_charts_test import BasePlaylistsChartsTest
+
+
+class TestBillboardChartsManager(BasePlaylistsChartsTest):
+    @fixture
+    def playlist_chart_map(self) -> Dict[str, Chart]:
+        return BILLBOARD_PLAYLIST_CHART_MAP
+
+    @fixture
+    async def job_builder(self) -> Type[BillboardChartJobBuilder]:
+        return BillboardChartJobBuilder
+
+    @fixture
+    def job_id(self) -> JobId:
+        return JobId.BILLBOARD_CHARTS
