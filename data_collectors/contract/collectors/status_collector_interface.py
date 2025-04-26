d5b6dd864b3f36aebb22650fe4b305705eef0e8a
nir.godin <nir.godin@planckresolution.com>
2025-04-26 18:27:50 +0300

    Change status collectors interface and align radio tracks collector to new interface

--- data_collectors/contract/collectors/status_collector_interface.py
+++ data_collectors/contract/collectors/status_collector_interface.py
@@ -1,16 +1,18 @@
 from abc import ABC, abstractmethod
 from datetime import timedelta
+from typing import List
 
 from sqlalchemy.ext.asyncio import AsyncEngine
 
 from data_collectors.contract.collector_interface import ICollector
+from data_collectors.logic.models.summary_section import SummarySection
 
 
 class IStatusCollector(ICollector, ABC):
     def __init__(self, db_engine: AsyncEngine):
         self._db_engine = db_engine
 
-    async def collect(self, lookback_period: timedelta) -> str:
+    async def collect(self, lookback_period: timedelta) -> List[SummarySection]:
         raise NotImplementedError
 
     @property
--- data_collectors/logic/collectors/misc/status_collectors/radio_tracks_status_collector.py
+++ data_collectors/logic/collectors/misc/status_collectors/radio_tracks_status_collector.py
@@ -1,22 +1,24 @@
 from datetime import timedelta, datetime
 from typing import List, Dict
 
+import pandas as pd
 from genie_common.utils import sort_dict_by_key
-from genie_datastores.postgres.models import RadioTrack, SpotifyStation
+from genie_datastores.postgres.models import RadioTrack
 from genie_datastores.postgres.operations import execute_query
 from sqlalchemy import select, func
 from sqlalchemy.engine import Row
 
 from data_collectors.consts.general_consts import DAY_LABEL
 from data_collectors.contract import IStatusCollector
+from data_collectors.logic.models.summary_section import SummarySection
 
 
 class RadioTracksStatusCollector(IStatusCollector):
-    async def collect(self, lookback_period: timedelta) -> str:
+    async def collect(self, lookback_period: timedelta) -> List[SummarySection]:
         records = await self._query_records(lookback_period)
         records_by_date = self._group_records_by_date(records)
 
-        return self._summarize(records_by_date)
+        return self._to_summary_sections(records_by_date)
 
     async def _query_records(self, lookback_period: timedelta) -> List[Row]:
         start_date = datetime.now() - lookback_period
@@ -36,33 +38,34 @@ class RadioTracksStatusCollector(IStatusCollector):
     @staticmethod
     def _group_records_by_date(
         records: List[Row],
-    ) -> Dict[datetime, Dict[SpotifyStation, int]]:
+    ) -> Dict[datetime, List[Dict[str, int]]]:
         grouped_records = {}
 
         for record in records:
             record_day: datetime = getattr(record, DAY_LABEL)
 
             if record_day not in grouped_records.keys():
-                grouped_records[record_day] = {}
+                grouped_records[record_day] = []
 
-            grouped_records[record_day][record.station] = record.count
+            formatted_record = {"Station": record.station.name, "Count": record.count}
+            grouped_records[record_day].append(formatted_record)
 
         return sort_dict_by_key(grouped_records, reverse=False)
 
     @staticmethod
-    def _summarize(records_by_date: Dict[datetime, Dict[SpotifyStation, int]]) -> str:
+    def _to_summary_sections(records_by_date: Dict[datetime, List[Dict[str, int]]]) -> List[SummarySection]:
         summaries = []
 
         for date, records in records_by_date.items():
-            date_summary = f"{date.strftime('%d/%m/%Y')}:"
+            summary_section = SummarySection(
+                title=date.strftime("%d/%m/%Y"),
+                data=pd.DataFrame.from_records(records),
+            )
 
-            for station, count in records.items():
-                date_summary += f"\n{station.name}: {count}"
+            summaries.append(summary_section)
 
-            summaries.append(date_summary)
-
-        return "\n\n".join(summaries)
+        return summaries
 
     @property
     def name(self) -> str:
-        return "Radio Tracks"
+        return "Daily Play Counts per Station"
