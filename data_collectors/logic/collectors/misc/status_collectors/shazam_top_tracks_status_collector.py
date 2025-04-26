5e651b2c1c3dae94607197f30c29351a243241ca
nir.godin <nir.godin@planckresolution.com>
2025-04-26 18:32:45 +0300

    Align shazam_top_tracks_status_collector to new interface

--- data_collectors/logic/collectors/misc/status_collectors/shazam_top_tracks_status_collector.py
+++ data_collectors/logic/collectors/misc/status_collectors/shazam_top_tracks_status_collector.py
@@ -1,6 +1,7 @@
 from datetime import timedelta, datetime
 from typing import List, Dict
 
+import pandas as pd
 from genie_common.utils import sort_dict_by_key
 from genie_datastores.postgres.models import ShazamTopTrack, ShazamLocation
 from genie_datastores.postgres.operations import execute_query
@@ -9,14 +10,15 @@ from sqlalchemy.engine import Row
 
 from data_collectors.consts.general_consts import DAY_LABEL
 from data_collectors.contract import IStatusCollector
+from data_collectors.logic.models.summary_section import SummarySection
 
 
 class ShazamTopTracksStatusCollector(IStatusCollector):
-    async def collect(self, lookback_period: timedelta) -> str:
+    async def collect(self, lookback_period: timedelta) -> List[SummarySection]:
         records = await self._query_records(lookback_period)
         records_by_date = self._group_records_by_date(records)
 
-        return self._summarize(records_by_date)
+        return self._to_summary_sections(records_by_date)
 
     async def _query_records(self, lookback_period: timedelta) -> List[Row]:
         start_date = datetime.now() - lookback_period
@@ -36,32 +38,32 @@ class ShazamTopTracksStatusCollector(IStatusCollector):
     @staticmethod
     def _group_records_by_date(
         records: List[Row],
-    ) -> Dict[datetime, Dict[ShazamLocation, int]]:
+    ) -> Dict[datetime, List[Dict[str, int]]]:
         grouped_records = {}
 
         for record in records:
             record_day: datetime = getattr(record, DAY_LABEL)
 
             if record_day not in grouped_records.keys():
-                grouped_records[record_day] = {}
+                grouped_records[record_day] = []
 
-            grouped_records[record_day][record.location] = record.count
+            formatted_record = {"Location": record.location.name, "Count": record.count}
+            grouped_records[record_day].append(formatted_record)
 
         return sort_dict_by_key(grouped_records, reverse=False)
 
     @staticmethod
-    def _summarize(records_by_date: Dict[datetime, Dict[ShazamLocation, int]]) -> str:
+    def _to_summary_sections(records_by_date: Dict[datetime, List[Dict[str, int]]]) -> List[SummarySection]:
         summaries = []
 
         for date, records in records_by_date.items():
-            date_summary = f"{date.strftime('%d/%m/%Y')}:"
+            section = SummarySection(
+                title=date.strftime("%d/%m/%Y"),
+                data=pd.DataFrame.from_records(records),
+            )
+            summaries.append(section)
 
-            for location, count in records.items():
-                date_summary += f"\n{location.name}: {count}"
-
-            summaries.append(date_summary)
-
-        return "\n\n".join(summaries)
+        return summaries
 
     @property
     def name(self) -> str:
