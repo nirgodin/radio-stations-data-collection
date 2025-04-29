efbddd8d681cba0b6aecc63713c12e9cf65f1058
nir.godin <nir.godin@planckresolution.com>
2025-04-29 21:17:49 +0300

    Added glglz consts

--- data_collectors/consts/glglz_consts.py
+++ data_collectors/consts/glglz_consts.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from itertools import product
 
 GLGLZ_WEEKLY_CHART_URL_FORMAT = "https://glz.co.il/%D7%92%D7%9C%D7%92%D7%9C%D7%A6/%D7%9B%D7%AA%D7%91%D7%95%D7%AA/%D7%94%D7%9E%D7%A6%D7%A2%D7%93-%D7%94%D7%A9%D7%91%D7%95%D7%A2%D7%99-{date}"
@@ -18,5 +17,8 @@ INTERNATIONAL_CHART_TITLE = b"\xd7\x94\xd7\x9e\xd7\xa6\xd7\xa2\xd7\x93 \xd7\x94\
 )
 GLGLZ_CHART_ENTRY = "glglz_chart_entry"
 POSITION_TRACK_NAME_SEPARATOR = "."
-FIRST_GLGLZ_CHART_DATE = datetime(2013, 1, 3)
 CHART_NAME_SIMILARITY_THRESHOLD = 0.8
+WEEKLY_CHART_PREFIX = (
+    b"\xd7\x94\xd7\x9e\xd7\xa6\xd7\xa2\xd7\x93 \xd7\x94\xd7\xa9\xd7\x91\xd7\x95\xd7\xa2\xd7\x99".decode("utf-8")
+)
+GLGLZ_CHARTS_ARCHIVE_URL = "https://glz.co.il/%D7%92%D7%9C%D7%92%D7%9C%D7%A6/%D7%9B%D7%AA%D7%91%D7%95%D7%AA/%D7%90%D7%A8%D7%9B%D7%99%D7%95%D7%9F-%D7%9E%D7%A6%D7%A2%D7%93%D7%99%D7%9D"
