3198985e93bc5c49dea11c57d0fee181f6aea1cf
nir.godin <nir.godin@planckresolution.com>
2025-06-05 20:11:09 +0300

    Initialize artists insights job builder

---
+++ data_collectors/jobs/job_builders/artists_insights_job_builder.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.util import undefined
+from typing_extensions import Optional
+
+from data_collectors.jobs.base_job_builder import BaseJobBuilder
+from data_collectors.jobs.job_id import JobId
+from data_collectors.logic.models import ScheduledJob
+from data_collectors.utils.datetime import random_upcoming_time
+
+
+class ArtistsInsightsJobBuilder(BaseJobBuilder):
+    async def build(self, next_run_time: Optional[datetime] = undefined) -> ScheduledJob:
+        return ScheduledJob(
+            task=self._task,
+            id=JobId.ARTISTS_INSIGHTS,
+            interval=IntervalTrigger(hours=12),
+            next_run_time=next_run_time or random_upcoming_time(),
+        )
+
+    async def _task(self) -> None:
+        manager = self._component_factory.google.get_wikipedia_artists_about_manager()
+        await manager.run(limit=100)
