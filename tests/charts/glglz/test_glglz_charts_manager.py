ce4abc72ba11dcf65ef6e7a9d09bedf56d3b646f
nir.godin <nir.godin@planckresolution.com>
2025-08-08 11:24:26 +0300

    Finalize glglz test

--- tests/charts/glglz/test_glglz_charts_manager.py
+++ tests/charts/glglz/test_glglz_charts_manager.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from functools import partial
 from http import HTTPStatus
 from random import randint, shuffle, choice
 from typing import Optional, Dict, List
@@ -16,10 +17,13 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncEngine
 from starlette.testclient import TestClient
 
+from data_collectors.components import ComponentFactory
 from data_collectors.consts.glglz_consts import WEEKLY_CHART_PREFIX, GLGLZ_CHARTS_ARCHIVE_ROUTE
+from data_collectors.jobs.job_builders.charts.glglz_charts_job_builder import GlglzChartsJobBuilder
 from data_collectors.jobs.job_id import JobId
 from tests.charts.glglz.glglz_chart_resources import GlglzChartResources
 from tests.helpers.mock_generate_content_response import MockGenerateContentResponse
+from tests.testing_utils import build_scheduled_test_client, until
 from tests.tools.playwright_testkit import PlaywrightTestkit
 from tests.tools.spotify_insertions_verifier import SpotifyInsertionsVerifier
 
@@ -36,19 +40,66 @@ class TestGlglzChartsManager:
         spotify_test_client: SpotifyTestClient,
         spotify_insertions_verifier: SpotifyInsertionsVerifier,
     ):
+        self._given_expected_requests(
+            playwright_testkit=playwright_testkit,
+            charts_archive_page=charts_archive_page,
+            charts_resources=charts_resources,
+            mock_gemini_model=mock_gemini_model,
+            spotify_test_client=spotify_test_client,
+        )
+
+        with test_client as client:
+            actual = client.post(f"jobs/trigger/{JobId.GLGLZ_CHARTS.value}")
+
+        assert actual.status_code == HTTPStatus.OK
+        assert await self._are_expected_db_records_inserted(
+            charts_resources=charts_resources,
+            spotify_insertions_verifier=spotify_insertions_verifier,
+            db_engine=db_engine,
+        )
+
+    async def test_scheduled(
+        self,
+        db_engine: AsyncEngine,
+        charts_archive_page: str,
+        charts_resources: List[GlglzChartResources],
+        playwright_testkit: PlaywrightTestkit,
+        mock_gemini_model: AsyncMock,
+        spotify_test_client: SpotifyTestClient,
+        spotify_insertions_verifier: SpotifyInsertionsVerifier,
+        scheduled_test_client: TestClient,
+    ):
+        self._given_expected_requests(
+            playwright_testkit=playwright_testkit,
+            charts_archive_page=charts_archive_page,
+            charts_resources=charts_resources,
+            mock_gemini_model=mock_gemini_model,
+            spotify_test_client=spotify_test_client,
+        )
+        condition = partial(
+            self._are_expected_db_records_inserted,
+            charts_resources,
+            spotify_insertions_verifier,
+            db_engine,
+        )
+
+        with scheduled_test_client:
+            await until(condition)
+
+    def _given_expected_requests(
+        self,
+        playwright_testkit: PlaywrightTestkit,
+        charts_archive_page: str,
+        charts_resources: List[GlglzChartResources],
+        mock_gemini_model: AsyncMock,
+        spotify_test_client: SpotifyTestClient,
+    ) -> None:
         self._expect_charts_archive_request(playwright_testkit, charts_archive_page)
         self._expect_charts_pages_requests(playwright_testkit, charts_resources)
         self._given_valid_gemini_responses(mock_gemini_model, charts_resources)
         self._given_valid_search_responses(spotify_test_client, charts_resources)
         self._given_valid_artists_responses(spotify_test_client, charts_resources)
         self._given_valid_audio_features_responses(spotify_test_client, charts_resources)
-
-        with test_client as client:
-            actual = client.post(f"jobs/trigger/{JobId.GLGLZ_CHARTS.value}")
-
-        assert actual.status_code == HTTPStatus.OK
-        assert await self._are_expected_spotify_entries_inserted(charts_resources, spotify_insertions_verifier)
-        assert await self._are_chart_entries_records_inserted(charts_resources, db_engine)
 
     @staticmethod
     def _expect_charts_archive_request(playwright_testkit: PlaywrightTestkit, charts_archive_page: str) -> None:
@@ -89,20 +140,6 @@ class TestGlglzChartsManager:
         tld = choice(["com", "net", "org", "io", "co"])
 
         return f"{protocol}://{subdomain}.{domain}.{tld}"
-
-    @fixture
-    def charts_archive_page(self, charts_resources: List[GlglzChartResources]) -> str:
-        glglz_links = [self._a_glglz_html_link(resource.date) for resource in charts_resources]
-        non_glglz_links = [self._an_html_link() for _ in range(randint(0, 10))]
-        links = glglz_links + non_glglz_links
-        shuffle(links)
-        formatted_links = "".join(links)
-
-        return f'<html><head><meta charset="UTF-8"></head><body>{formatted_links}</body></html>'
-
-    @fixture
-    def charts_resources(self) -> List[GlglzChartResources]:
-        return [GlglzChartResources.random()]
 
     @staticmethod
     def _given_valid_gemini_responses(
@@ -149,6 +186,21 @@ class TestGlglzChartsManager:
         tracks_ids = chain_lists([resource.get_tracks_ids() for resource in charts_resources])
         spotify_test_client.tracks.audio_features.expect_success(sorted(tracks_ids))
 
+    async def _are_expected_db_records_inserted(
+        self,
+        charts_resources: List[GlglzChartResources],
+        spotify_insertions_verifier: SpotifyInsertionsVerifier,
+        db_engine: AsyncEngine,
+    ) -> bool:
+        are_spotify_records_inserted = await self._are_expected_spotify_entries_inserted(
+            charts_resources, spotify_insertions_verifier
+        )
+
+        if are_spotify_records_inserted:
+            return await self._are_chart_entries_records_inserted(charts_resources, db_engine)
+
+        return False
+
     @staticmethod
     async def _are_expected_spotify_entries_inserted(
         charts_resources: List[GlglzChartResources], spotify_insertions_verifier: SpotifyInsertionsVerifier
@@ -175,3 +227,27 @@ class TestGlglzChartsManager:
         actual = query_result.scalars().all()
 
         return sorted(actual) == sorted(expected)
+
+    @fixture
+    def charts_archive_page(self, charts_resources: List[GlglzChartResources]) -> str:
+        glglz_links = [self._a_glglz_html_link(resource.date) for resource in charts_resources]
+        non_glglz_links = [self._an_html_link() for _ in range(randint(0, 10))]
+        links = glglz_links + non_glglz_links
+        shuffle(links)
+        formatted_links = "".join(links)
+
+        return f'<html><head><meta charset="UTF-8"></head><body>{formatted_links}</body></html>'
+
+    @fixture
+    def charts_resources(self) -> List[GlglzChartResources]:
+        return [GlglzChartResources.random()]
+
+    @fixture
+    async def scheduled_test_client(
+        self,
+        component_factory: ComponentFactory,
+    ) -> TestClient:
+        scheduled_client = await build_scheduled_test_client(component_factory, GlglzChartsJobBuilder)
+
+        with scheduled_client as client:
+            yield client
