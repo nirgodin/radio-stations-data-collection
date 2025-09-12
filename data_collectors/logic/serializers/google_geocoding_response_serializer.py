603f22162638ad6d458961d14c53336ff0339dd0
nir.godin <nir.godin@planckresolution.com>
2025-09-12 09:52:30 +0300

    Make geocoding response serializer return empty update request upon failure

--- data_collectors/logic/serializers/google_geocoding_response_serializer.py
+++ data_collectors/logic/serializers/google_geocoding_response_serializer.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional, Dict, List
 
 from genie_common.utils import safe_nested_get
@@ -18,22 +19,22 @@ from data_collectors.logic.models import DBUpdateRequest, AddressComponentSettin
 
 class GoogleGeocodingResponseSerializer(ISerializer):
     def serialize(self, artist_id: str, geocoding: Optional[dict]) -> Optional[DBUpdateRequest]:
-        if geocoding is None:
-            return
-
         first_result = self._extract_geocoding_first_result(geocoding)
+        update_values = {Artist.update_date: datetime.utcnow()}
+
         if first_result is not None:
             update_values = self._serialize_address_components(first_result)
             self._add_latitude_and_longitude_to_update_values(update_values, first_result)
 
-            return DBUpdateRequest(id=artist_id, values=update_values)
+        return DBUpdateRequest(id=artist_id, values=update_values)
 
     @staticmethod
-    def _extract_geocoding_first_result(geocoding: dict) -> Optional[dict]:
-        results = geocoding.get(RESULTS, [])
+    def _extract_geocoding_first_result(geocoding: Optional[dict]) -> Optional[dict]:
+        if isinstance(geocoding, dict):
+            results = geocoding.get(RESULTS, [])
 
-        if results and isinstance(results, list):
-            return results[0]
+            if results and isinstance(results, list):
+                return results[0]
 
     def _serialize_address_components(self, geocoding_result: dict) -> Dict[Artist, str]:
         components = {}
