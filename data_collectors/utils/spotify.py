a87cb44e41f3016d74f34064302bd9d0acaa9786
nir.godin <nir.godin@planckresolution.com>
2025-09-15 15:20:00 +0300

    Fix AttributeError during artists extraction

--- data_collectors/utils/spotify.py
+++ data_collectors/utils/spotify.py
@@ -1,9 +1,11 @@
-from typing import Set, List
+from typing import Set, List, Optional
+
+from genie_common.utils import safe_nested_get
 
 from data_collectors.consts.spotify_consts import TRACK, ARTISTS, ID
 
 
-def extract_unique_artists_ids(*tracks: dict) -> Set[str]:
+def extract_unique_artists_ids(*tracks: Optional[dict]) -> Set[str]:
     artists_ids = set()
 
     for track in tracks:
@@ -15,10 +17,13 @@ def extract_unique_artists_ids(*tracks: dict) -> Set[str]:
     return artists_ids
 
 
-def get_track_artists(track: dict) -> List[dict]:
-    return track.get(TRACK, {}).get(ARTISTS, [])
+def get_track_artists(track: Optional[dict]) -> List[dict]:
+    if isinstance(track, dict):
+        return safe_nested_get(track, [TRACK, ARTISTS], [])
+
+    return []
 
 
-def _extract_single_artist_id(track: dict) -> List[str]:
+def _extract_single_artist_id(track: Optional[dict]) -> List[str]:
     artists = get_track_artists(track)
     return [artist[ID] for artist in artists]
