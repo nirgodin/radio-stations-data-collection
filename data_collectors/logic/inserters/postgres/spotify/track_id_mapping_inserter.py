725a55e4239969f17ee7243d1d4d5b312a6cc95a
nir.godin <nir.godin@planckresolution.com>
2025-09-22 16:43:21 +0300

    Fix more tracks inserters jobs

--- data_collectors/logic/inserters/postgres/spotify/track_id_mapping_inserter.py
+++ data_collectors/logic/inserters/postgres/spotify/track_id_mapping_inserter.py
@@ -1,10 +1,11 @@
-from typing import Type, List
+from typing import Type, List, Any
 
 from genie_datastores.postgres.models import TrackIDMapping
 from genie_datastores.postgres.models.orm.spotify.base_spotify_orm_model import (
     BaseSpotifyORMModel,
 )
 
+from data_collectors.consts.spotify_consts import TRACK, ID
 from data_collectors.logic.inserters.postgres.spotify.base_spotify_database_inserter import (
     BaseSpotifyDatabaseInserter,
 )
@@ -13,6 +14,15 @@ from data_collectors.logic.inserters.postgres.spotify.base_spotify_database_inse
 class TrackIDMappingDatabaseInserter(BaseSpotifyDatabaseInserter):
     async def _get_raw_records(self, tracks: List[dict]) -> List[dict]:
         return tracks
+
+    def _is_serializable(self, raw: Any) -> bool:
+        if isinstance(raw, dict):
+            inner_track = raw.get(TRACK)
+
+            if isinstance(inner_track, dict):
+                return isinstance(inner_track.get(ID), str)
+
+        return False
 
     @property
     def name(self) -> str:
