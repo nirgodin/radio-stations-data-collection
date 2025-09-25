3693a07cc026b5e57f8c0ea24c35d20441519dbd
nir.godin <nir.godin@planckresolution.com>
2025-09-25 19:42:49 +0300

    Initialize billboard logic

---
+++ data_collectors/consts/billboard_consts.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+BILLBOARD_DATETIME_FORMAT = "%Y-%m-%d"
+FIRST_BILLBOARD_CHART_DATE = datetime(1958, 8, 4)
---
+++ data_collectors/logic/managers/charts/billboard_charts_manager.py
@@ -0,0 +1,23 @@
+from datetime import timedelta, datetime
+from typing import Dict, List
+
+from genie_datastores.postgres.models import ChartEntry
+from genie_datastores.postgres.operations import execute_query
+from sqlalchemy import select
+
+from data_collectors.consts.billboard_consts import FIRST_BILLBOARD_CHART_DATE
+from data_collectors.logic.managers.charts.base_charts_manager import BaseChartsManager
+
+
+class BillboardChartsManager(BaseChartsManager):
+    async def _generate_data_collector_kwargs(self) -> Dict[str, List[datetime]]:
+        query = select(ChartEntry.date).order_by(ChartEntry.date.desc()).limit(1)
+        query_result = await execute_query(engine=self._db_engine, query=query)
+        last_chart_date = query_result.scalars().first()
+
+        if last_chart_date:
+            dates = [last_chart_date + timedelta(days=7)]
+        else:
+            dates = [FIRST_BILLBOARD_CHART_DATE]
+
+        return {"dates": dates}
