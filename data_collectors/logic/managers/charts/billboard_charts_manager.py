d0eefdd8b98f30d463c5d57e070d49fa15507c9c
nir.godin <nir.godin@planckresolution.com>
2025-09-26 10:36:21 +0300

    Use aggregate max function instead of manual query

--- data_collectors/logic/managers/charts/billboard_charts_manager.py
+++ data_collectors/logic/managers/charts/billboard_charts_manager.py
@@ -1,9 +1,9 @@
 from datetime import timedelta, datetime
 from typing import Dict, List
 
-from genie_datastores.postgres.models import ChartEntry
+from genie_datastores.postgres.models import ChartEntry, Chart
 from genie_datastores.postgres.operations import execute_query
-from sqlalchemy import select
+from sqlalchemy import select, func
 
 from data_collectors.consts.billboard_consts import FIRST_BILLBOARD_CHART_DATE
 from data_collectors.logic.managers.charts.base_charts_manager import BaseChartsManager
@@ -11,7 +11,7 @@ from data_collectors.logic.managers.charts.base_charts_manager import BaseCharts
 
 class BillboardChartsManager(BaseChartsManager):
     async def _generate_data_collector_kwargs(self) -> Dict[str, List[datetime]]:
-        query = select(ChartEntry.date).order_by(ChartEntry.date.desc()).limit(1)
+        query = select(func.max(ChartEntry.date)).where(ChartEntry.chart == Chart.BILLBOARD_HOT_100)
         query_result = await execute_query(engine=self._db_engine, query=query)
         last_chart_date = query_result.scalars().first()
 
