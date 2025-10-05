2d605d8ff0599f50f22678f1a83b6f113f0e0c65
nir.godin <nir.godin@planckresolution.com>
2025-10-05 15:38:45 +0300

    Fix billboard data collector

--- data_collectors/consts/billboard_consts.py
+++ data_collectors/consts/billboard_consts.py
@@ -1,5 +1,14 @@
 from datetime import datetime
 
+from data_collectors.logic.models import HTMLElement, WebElement
+
 BILLBOARD_DATETIME_FORMAT = "%Y-%m-%d"
 FIRST_BILLBOARD_CHART_DATE = datetime(1958, 8, 4)
 BILLBOARD_HOT_100_CHART_NAME = "hot-100"
+SONG_LIST_ITEM_WEB_ELEMENT = WebElement(
+    name="a",
+    type=HTMLElement.LI,
+    class_="o-chart-results-list__item // lrv-u-flex-grow-1 lrv-u-flex lrv-u-flex-direction-column lrv-u-justify-content-center lrv-u-padding-l-050 lrv-u-padding-l-00@mobile-max u-max-width-397",
+    multiple=True,
+    enumerate=False,
+)
