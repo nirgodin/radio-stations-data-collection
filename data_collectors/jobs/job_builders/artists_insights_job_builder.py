0da95bb5651ba82e698a344a40a6c8628299793f
nir.godin <nir.godin@planckresolution.com>
2025-10-10 11:09:09 +0300

    Enable artists insights job

--- data_collectors/jobs/job_builders/artists_insights_job_builder.py
+++ data_collectors/jobs/job_builders/artists_insights_job_builder.py
@@ -1,20 +1,22 @@
 from datetime import datetime
 
 from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.util import undefined
 from typing_extensions import Optional
 
 from data_collectors.jobs.base_job_builder import BaseJobBuilder
 from data_collectors.jobs.job_id import JobId
 from data_collectors.logic.models import ScheduledJob
+from data_collectors.utils.datetime import random_upcoming_time
 
 
 class ArtistsInsightsJobBuilder(BaseJobBuilder):
-    async def build(self, next_run_time: Optional[datetime] = None) -> ScheduledJob:
+    async def build(self, next_run_time: Optional[datetime] = undefined) -> ScheduledJob:
         return ScheduledJob(
             task=self._task,
             id=JobId.ARTISTS_INSIGHTS,
             interval=IntervalTrigger(hours=12),
-            next_run_time=next_run_time,
+            next_run_time=next_run_time or random_upcoming_time(),
         )
 
     async def _task(self) -> None:
