5c419061c6d2b319f946c41282863e500f45ccde
nir.godin <nir.godin@planckresolution.com>
2025-10-14 11:06:06 +0300

    Created curations_inserters_component_factory

---
+++ data_collectors/components/inserters/curations_inserters_component_factory.py
@@ -0,0 +1,30 @@
+from data_collectors.components.tools_component_factory import ToolsComponentFactory
+from data_collectors.logic.inserters.postgres import (
+    ChunksDatabaseInserter,
+    CuratorsDatabaseInserter,
+    CuratorsCollectionsDatabaseInserter,
+    CuratedTracksDatabaseInserter,
+)
+
+
+class CurationsInsertersComponentFactory:
+    def __init__(self, tools: ToolsComponentFactory):
+        self._tools = tools
+
+    def get_curators_inserter(self) -> CuratorsDatabaseInserter:
+        return CuratorsDatabaseInserter(db_engine=self._tools.get_database_engine())
+
+    def get_curators_collections_inserter(self) -> CuratorsCollectionsDatabaseInserter:
+        return CuratorsCollectionsDatabaseInserter(db_engine=self._tools.get_database_engine())
+
+    def get_curated_tracks_inserter(self) -> CuratedTracksDatabaseInserter:
+        return CuratedTracksDatabaseInserter(
+            db_engine=self._tools.get_database_engine(), chunks_inserter=self._get_chunks_database_inserter()
+        )
+
+    def _get_chunks_database_inserter(self) -> ChunksDatabaseInserter:
+        chunks_generator = self._tools.get_chunks_generator()
+        return ChunksDatabaseInserter(
+            db_engine=self._tools.get_database_engine(),
+            chunks_generator=chunks_generator,
+        )
