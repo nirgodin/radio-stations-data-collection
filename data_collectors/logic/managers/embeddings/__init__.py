962a43e2ec7bd63a2d3bab41b08e1040067ff8fa
nir.godin <nir.godin@planckresolution.com>
2025-11-08 15:32:57 +0200

    Remove track names embeddings logic

--- data_collectors/logic/managers/embeddings/__init__.py
+++
@@ -1,8 +0,0 @@
-from data_collectors.logic.managers.embeddings.track_names_embeddings_manager import (
-    TrackNamesEmbeddingsManager,
-)
-from data_collectors.logic.managers.embeddings.track_names_embeddings_retrieval_manager import (
-    TrackNamesEmbeddingsRetrievalManager,
-)
-
-__all__ = ["TrackNamesEmbeddingsManager", "TrackNamesEmbeddingsRetrievalManager"]
